2494f1b5ea2bc7cdb331c217a101396e81299552
Earthmark <earthmark.miller@gmail.com>
2026-07-30 18:50:28 -0700

    Added missing hash and equality members for classes that implemented equality comparers. Hash codes for rotations are not implemented well.

--- CodeGenerationConfig/Config.cs
+++ CodeGenerationConfig/Config.cs
@@ -46,7 +46,15 @@ namespace CodeGenerationConfig
             this.Columns = columns;
         }
 
-        public bool Equals(MatrixSize other) => Rows == other.Rows && Columns == other.Columns;
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((MatrixSize)obj);
+        }
+
+        public bool Equals(MatrixSize other) => other != null && Rows == other.Rows && Columns == other.Columns;
 
         public static bool operator ==(MatrixSize left, MatrixSize right)
         {
@@ -60,6 +68,14 @@ namespace CodeGenerationConfig
         }
 
         public static bool operator !=(MatrixSize left, MatrixSize right) => !(left == right);
+        
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Rows * 397) ^ Columns;
+            }
+        }
     }
 
     public static class Config
