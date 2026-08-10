f1d1256f13072438879de8732e0c2948235131dd
Frooxius <tomas.mariancik@gmail.com>
2026-08-10 18:59:22 +0200

    feat: Add Corner enum

---
+++ Elements.Core/Enums/Corner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elements.Data;
+
+namespace Elements.Core
+{
+    /// <summary>
+    /// Indicates positioning in one of the corners of a square
+    /// </summary>
+    [DataModelType]
+    public enum Corner
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight
+    }
+}
